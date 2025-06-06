9452576ca2bbac8c3801b51726aac1dca9451b94
Naveen K M <136746420+naveeenkm@users.noreply.github.com>
2025-06-06 16:08:00 +0530

    Update settings.py

--- iot_tracker/iot_tracker/settings.py
+++ iot_tracker/iot_tracker/settings.py
@@ -105,11 +105,11 @@ WSGI_APPLICATION = "iot_tracker.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': '3306',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'pUPXmcILWubJHZWDAspBRXLAhfWWrPNY',
+        'HOST': 'yamabiko.proxy.rlwy.net',
+        'PORT': '33725',
     }
 }
 
